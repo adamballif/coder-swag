5a9d2f0eec0216abaf51b5a5d2437a7774de9f50
Adam Ballif <adamballif@Adams-15-MacBook-Pro.local>
2019-02-03 22:06:56 -0700

    prototype cell created

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Adam Ballif on 2/3/19.
+//  Copyright © 2019 Adam Ballif. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
