c8d4d6d80744c109c90d60471ba4d08de88f5b7d
Adam Ballif <adamballif@Adams-15-MacBook-Pro.local>
2019-02-04 21:21:29 -0700

    collection view

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Adam Ballif on 2/4/19.
+//  Copyright © 2019 Adam Ballif. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
